2ea60e8d6bffb1620ff0c0f64201d600438046e4
Tristan Sloughter <tristan.sloughter@gmail.com>
2012-04-29 14:02:50 -0500

    added eval handler

---
+++ lib/mmmbot/src/mmmbot_eval.erl
@@ -0,0 +1,128 @@
+%%%-------------------------------------------------------------------
+%%% @author Tristan Sloughter <>
+%%% @copyright (C) 2012, Tristan Sloughter
+%%% @doc
+%%%
+%%% @end
+%%% Created : 29 Apr 2012 by Tristan Sloughter <>
+%%%-------------------------------------------------------------------
+-module(mmmbot_eval).
+
+-behaviour(gen_event).
+
+%% API
+-export([start/0]).
+
+%% gen_event callbacks
+-export([init/1, handle_event/2, handle_call/2, 
+         handle_info/2, terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE). 
+
+-record(state, {funs=[]}).
+
+%%%===================================================================
+%%% gen_event callbacks
+%%%===================================================================
+
+start() ->
+    mmmbot_em:add_handler(?SERVER).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a new event handler is added to an event manager,
+%% this function is called to initialize the event handler.
+%%
+%% @spec init(Args) -> {ok, State}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    {ok, #state{}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever an event manager receives an event sent using
+%% gen_event:notify/2 or gen_event:sync_notify/2, this function is
+%% called for each installed event handler to handle the event.
+%%
+%% @spec handle_event(Event, State) ->
+%%                          {ok, State} |
+%%                          {swap_handler, Args1, State1, Mod2, Args2} |
+%%                          remove_handler
+%% @end
+%%--------------------------------------------------------------------
+handle_event({Line="fun() ->" ++ _, _User}, State=#state{funs=Funs}) ->
+    {ok, Tokens, _} = erl_scan:string(Line),
+    {ok, [Form]} = erl_parse:parse_exprs(Tokens),
+    {ok, State#state{funs=[Form | Funs]}};
+handle_event({Line, User}, State=#state{funs=Funs}) ->
+    lists:foreach(fun(Form) ->
+                          Bindings =  erl_eval:add_binding('User', User, 
+                                     erl_eval:add_binding('Msg', Line, erl_eval:new_bindings())),
+                          {value, Fun, _} = erl_eval:expr(Form, Bindings),   
+                          Fun()
+                  end, Funs),
+    {ok, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever an event manager receives a request sent using
+%% gen_event:call/3,4, this function is called for the specified
+%% event handler to handle the request.
+%%
+%% @spec handle_call(Request, State) ->
+%%                   {ok, Reply, State} |
+%%                   {swap_handler, Reply, Args1, State1, Mod2, Args2} |
+%%                   {remove_handler, Reply}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(_Request, State) ->
+    Reply = ok,
+    {ok, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called for each installed event handler when
+%% an event manager receives any other message than an event or a
+%% synchronous request (or a system message).
+%%
+%% @spec handle_info(Info, State) ->
+%%                         {ok, State} |
+%%                         {swap_handler, Args1, State1, Mod2, Args2} |
+%%                         remove_handler
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {ok, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever an event handler is deleted from an event manager, this
+%% function is called. It should be the opposite of Module:init/1 and
+%% do any necessary cleaning up.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
